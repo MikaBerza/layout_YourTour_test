4f3c35f849230fa939bcf3bf98fada27a93df14d
Mika <mikberza@mail.ru>
2023-06-12 21:35:55 +0300

    Внесение исправлений в макет раздела (header)
    Making corrections to the layout of the section (header)

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,8 @@ import '../css/fonts.css';
 import '../css/index.css';
 import '../css/normalizeStyle.css';
 
-import '../assets/images/headerBackgroundImg.png';
-import '../assets/images/logoYourTour.png';
-import '../assets/images/telephoneNumberIcon.png'
+// import '../assets/images/headerBackgroundImg1920.svg';
+import '../assets/images/headerBackgroundImg1920.png';
+import '../assets/images/logoYourTourWhite.png';
+// import '../assets/images/logoYourTourBlack.png';
+// import '../assets/images/telephoneNumberIcon.png'
