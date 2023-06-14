5148a8083b544c9fb4bc162c6ef70561112fc216
Mika <mikberza@mail.ru>
2023-06-14 16:25:37 +0300

    Верстка следующих разделов завершена -верстка раздела (heading) завершена -верстка раздела (greeting) завершена
    Layout of the following sections is completed
    -layout of the section (heading) is completed
    -layout of the section (greeting) is completed

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,38 @@
-import '../css/fonts.css';
 import '../css/index.css';
-// import '../css/indexDDD.css';
-import '../css/normalizeStyle.css';
 
 import '../assets/images/headerBackgroundImg1920.png';
 
-// // контейнер шапки навигационной панели
-// const navbarContainer = document.querySelector('.navbar-container');
-// //
-// const navbarLogoYourTour = document.querySelector('.navbar__logo_YourTour');
-// //
-// const navbarMenuLink = document.querySelectorAll('.navbar__menu-item');
-// console.log(navbarMenuLink);
-// //
-// const navbarTelephoneNumber = document.querySelector(
-//   '.navbar__telephoneNumber'
-// );
+const heading = document.querySelector('.heading');
+const logoYourTour = heading.querySelector('.navbar__logo_YourTour');
+const navbarMenuLink = heading.querySelectorAll('.navbar__menu-link');
+const telephoneNumber = heading.querySelector('.navbar__telephoneNumber');
 
-// window.addEventListener('scroll', function () {
-//   // если пользователь прокручивает на 831px и больше, то применяем стили
-//   if (scrollY > 831) {
-//     navbarContainer.classList.add('scrolled-background');
-//     navbarLogoYourTour.classList.add('scrolled-color');
-//     for (let elem of navbarMenuLink) {
-//       elem.firstElementChild.classList.add('scrolled-color');
-//     }
-//     navbarTelephoneNumber.classList.add('scrolled-color');
-//   } else {
-//     navbarContainer.classList.remove('scrolled-background');
-//     navbarLogoYourTour.classList.remove('scrolled-color');
-//     for (let elem of navbarMenuLink) {
-//       elem.firstElementChild.classList.remove('scrolled-color');
-//     }
-//     navbarTelephoneNumber.classList.remove('scrolled-color');
-//   }
-// });
+// добавить стиль навигационной панели
+function addNavbarStyle() {
+  heading.classList.add('scrolled-bgn');
+  logoYourTour.classList.add('scrolled-color');
+  for (let i = 0; i < navbarMenuLink.length; i++) {
+    navbarMenuLink[i].classList.add('scrolled-color');
+    navbarMenuLink[i].classList.add('scrolled-hover');
+  }
+  telephoneNumber.classList.add('scrolled-color');
+}
+
+// удалить стиль навигационной панели
+function removeNavbarStyle() {
+  heading.classList.remove('scrolled-bgn');
+  logoYourTour.classList.remove('scrolled-color');
+  for (let i = 0; i < navbarMenuLink.length; i++) {
+    navbarMenuLink[i].classList.remove('scrolled-color');
+    navbarMenuLink[i].classList.remove('scrolled-hover');
+  }
+  telephoneNumber.classList.remove('scrolled-color');
+}
+
+window.addEventListener('scroll', () => {
+  if (scrollY > 766) {
+    addNavbarStyle();
+  } else {
+    removeNavbarStyle();
+  }
+});
