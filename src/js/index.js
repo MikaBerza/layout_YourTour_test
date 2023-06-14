759afca42b513b59db25cc4059881ebc4bffac03
Mika <mikberza@mail.ru>
2023-06-14 12:15:18 +0300

    Верстка до изменений

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,37 @@
 import '../css/fonts.css';
 import '../css/index.css';
+// import '../css/indexDDD.css';
 import '../css/normalizeStyle.css';
 
-// import '../assets/images/headerBackgroundImg1920.svg';
 import '../assets/images/headerBackgroundImg1920.png';
-import '../assets/images/logoYourTourWhite.png';
-// import '../assets/images/logoYourTourBlack.png';
-// import '../assets/images/telephoneNumberIcon.png'
+
+// // контейнер шапки навигационной панели
+// const navbarContainer = document.querySelector('.navbar-container');
+// //
+// const navbarLogoYourTour = document.querySelector('.navbar__logo_YourTour');
+// //
+// const navbarMenuLink = document.querySelectorAll('.navbar__menu-item');
+// console.log(navbarMenuLink);
+// //
+// const navbarTelephoneNumber = document.querySelector(
+//   '.navbar__telephoneNumber'
+// );
+
+// window.addEventListener('scroll', function () {
+//   // если пользователь прокручивает на 831px и больше, то применяем стили
+//   if (scrollY > 831) {
+//     navbarContainer.classList.add('scrolled-background');
+//     navbarLogoYourTour.classList.add('scrolled-color');
+//     for (let elem of navbarMenuLink) {
+//       elem.firstElementChild.classList.add('scrolled-color');
+//     }
+//     navbarTelephoneNumber.classList.add('scrolled-color');
+//   } else {
+//     navbarContainer.classList.remove('scrolled-background');
+//     navbarLogoYourTour.classList.remove('scrolled-color');
+//     for (let elem of navbarMenuLink) {
+//       elem.firstElementChild.classList.remove('scrolled-color');
+//     }
+//     navbarTelephoneNumber.classList.remove('scrolled-color');
+//   }
+// });
