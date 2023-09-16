c0bb325149722607ac8840d47daf94df9fc72139
Mika <mikberza@mail.ru>
2023-09-16 21:48:58 +0300

    Настройка Stylelint - настроен Stylelint - создан файл конфигурации (.stylelintrc.js) - настроен webpack для Stylelint
    Setting up Stylelint
    - stylelint is configured
    - a configuration file has been created (.stylelintrc.js )
    - configured webpack for Stylelint

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const StylelintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -37,6 +38,12 @@ module.exports = {
           to: 'images',
         },
       ],
+    }),
+    new StylelintPlugin({
+      configFile: '.stylelintrc.js',
+      context: 'src',
+      files: '**/*.css',
+      configBasedir: __dirname,
     }),
   ],
   module: {
