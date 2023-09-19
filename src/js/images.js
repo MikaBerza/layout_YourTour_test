7798907cbe6934782a121fd1429fb0b74eb218ef
Mika <mikberza@mail.ru>
2023-09-20 02:48:27 +0300

    Удаление комментариев и импортов (раздел images) Deleting comments and imports (images section)

--- src/js/images.js
+++ src/js/images.js
@@ -14,71 +14,12 @@ import '../assets/images/fav_icons_YT/mstile-558x270.png';
 import '../assets/images/fav_icons_YT/mstile-558x558.png';
 // импортируем изображение для раздела ("greeting")
 import '../assets/images/greeting/greetingBackgroundImg.png';
-// import '../assets/images/greeting/greetingBackgroundImg1024.png';
 // импортируем изображение для раздела ("choose-tour")
-// import photo1 from '../assets/images/chooseTour/card_tour_photo_1.png';
-// import photo2 from '../assets/images/chooseTour/card_tour_photo_2.png';
-// import photo3 from '../assets/images/chooseTour/card_tour_photo_3.png';
-// import photo4 from '../assets/images/chooseTour/card_tour_photo_4.png';
-// import photo5 from '../assets/images/chooseTour/card_tour_photo_5.png';
-// import photo6 from '../assets/images/chooseTour/card_tour_photo_6.png';
 import '../assets/images/VectorRight.svg';
 // импортируем изображение для раздела ("collect-tour")
 import '../assets/images/VectorDown.svg';
 // импортируем изображение для раздела ("reviews")
 import '../assets/images/reviewPhoto1.png';
 import '../assets/images/reviewPhoto2.png';
-// импортируем изображение для раздела ("travelPhotos")
-// import travelPhoto1 from '../assets/images/travelPhotos/travelPhoto1.png';
-// import travelPhoto2 from '../assets/images/travelPhotos/travelPhoto2.png';
-// import travelPhoto3 from '../assets/images/travelPhotos/travelPhoto3.png';
-// import travelPhoto4 from '../assets/images/travelPhotos/travelPhoto4.png';
-// import travelPhoto5 from '../assets/images/travelPhotos/travelPhoto5.png';
-// import travelPhoto6 from '../assets/images/travelPhotos/travelPhoto6.png';
-// import travelPhoto7 from '../assets/images/travelPhotos/travelPhoto7.png';
-// import travelPhoto8 from '../assets/images/travelPhotos/travelPhoto8.png';
-// import travelPhoto9 from '../assets/images/travelPhotos/travelPhoto9.png';
-// import travelPhoto10 from '../assets/images/travelPhotos/travelPhoto10.png';
-// import travelPhoto11 from '../assets/images/travelPhotos/travelPhoto11.png';
-// import travelPhoto12 from '../assets/images/travelPhotos/travelPhoto12.png';
-// import travelPhoto13 from '../assets/images/travelPhotos/travelPhoto13.png';
-// импортируем изображение для раздела ("stories")
-// import storiesPhoto1 from '../assets/images/stories/storyPhoto1.png';
-// import storiesPhoto2 from '../assets/images/stories/storyPhoto2.png';
-// import storiesPhoto3 from '../assets/images/stories/storyPhoto3.png';
 // импортируем изображение для раздела ("footers")
 import '../assets/images/footersPhoto.png';
-
-// экспортируем массив с ссылками на изображения из ("choose-tour")
-// export const arrayLinksToImagesChooseTour = [
-//   photo1,
-//   photo2,
-//   photo3,
-//   photo4,
-//   photo5,
-//   photo6,
-// ];
-
-// экспортируем массив с ссылками на изображения из ("travelPhotos")
-// export const arrayLinksToImagesTravelPhotos1 = [
-//   travelPhoto1,
-//   travelPhoto2,
-//   travelPhoto3,
-//   travelPhoto4,
-//   travelPhoto5,
-//   travelPhoto6,
-//   travelPhoto7,
-//   travelPhoto8,
-//   travelPhoto9,
-//   travelPhoto10,
-//   travelPhoto11,
-//   travelPhoto12,
-//   travelPhoto13,
-// ];
-
-// массив с ссылками на изображения из ("stories 1920px")
-// export const arrayLinksToImagesStories1 = [
-//   storiesPhoto1,
-//   storiesPhoto2,
-//   storiesPhoto3,
-// ];
