cbf05f5a14d8adff36dc771d3f6d6f4fc792edc3
Mika <mikberza@mail.ru>
2023-09-22 15:43:46 +0300

    Удаление комментариев и оптимизация -удаление длинных функций из css -удаление комментариев из файлов css -оптимизация кода css (записей margin, padding)
    Deleting comments and optimizing
    -removing long functions from css
    -removing comments from css files
    -optimization of css code (margin, padding entries)

--- src/js/images.js
+++ src/js/images.js
@@ -15,11 +15,35 @@ import '../assets/images/fav_icons_YT/mstile-558x558.png';
 // импортируем изображение для раздела ("greeting")
 import '../assets/images/greeting/greetingBackgroundImg.png';
 // импортируем изображение для раздела ("choose-tour")
+import '../assets/images/chooseTour/card_tour_photo_1.png';
+import '../assets/images/chooseTour/card_tour_photo_2.png';
+import '../assets/images/chooseTour/card_tour_photo_3.png';
+import '../assets/images/chooseTour/card_tour_photo_4.png';
+import '../assets/images/chooseTour/card_tour_photo_5.png';
+import '../assets/images/chooseTour/card_tour_photo_6.png';
 import '../assets/images/VectorRight.svg';
 // импортируем изображение для раздела ("collect-tour")
 import '../assets/images/VectorDown.svg';
 // импортируем изображение для раздела ("reviews")
 import '../assets/images/reviewPhoto1.png';
 import '../assets/images/reviewPhoto2.png';
+// импортируем изображение для раздела ("travel-photos")
+import '../assets/images/travelPhotos/travelPhoto1.png';
+import '../assets/images/travelPhotos/travelPhoto2.png';
+import '../assets/images/travelPhotos/travelPhoto3.png';
+import '../assets/images/travelPhotos/travelPhoto4.png';
+import '../assets/images/travelPhotos/travelPhoto5.png';
+import '../assets/images/travelPhotos/travelPhoto6.png';
+import '../assets/images/travelPhotos/travelPhoto7.png';
+import '../assets/images/travelPhotos/travelPhoto8.png';
+import '../assets/images/travelPhotos/travelPhoto9.png';
+import '../assets/images/travelPhotos/travelPhoto10.png';
+import '../assets/images/travelPhotos/travelPhoto11.png';
+import '../assets/images/travelPhotos/travelPhoto12.png';
+import '../assets/images/travelPhotos/travelPhoto13.png';
+// импортируем изображение для раздела ("stories")
+import '../assets/images/stories/storyPhoto1.png';
+import '../assets/images/stories/storyPhoto2.png';
+import '../assets/images/stories/storyPhoto3.png';
 // импортируем изображение для раздела ("footers")
 import '../assets/images/footersPhoto.png';
